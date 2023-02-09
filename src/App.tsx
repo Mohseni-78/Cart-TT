c6074f4222b85134a409be22ccaa5588ed505300
Alireza Mohseni <mohseni.arz@gmail.com>
2023-02-09 17:13:40 +0330

    get Api in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
+import Sidebar from "./components/Sidebar";
+import { getProducts } from "./services/api";
 function App() {
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setData(await getProducts());
+    };
+    fetchApi();
+  }, []);
+
   return (
     <>
-  <Navbar/>
-  <Products/>
+      <Navbar setShowSidebar={setShowSidebar} />
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+      <Products products={data}/>
     </>
   );
 }
